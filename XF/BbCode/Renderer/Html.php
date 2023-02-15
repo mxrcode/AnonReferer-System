5f2682becde8defce5c2b31c136de0c910989028
mxrcode <123785508+mxrcode@users.noreply.github.com>
2023-02-15 04:15:55 +0200

    Update Html.php
    trash del

--- XF/BbCode/Renderer/Html.php
+++ XF/BbCode/Renderer/Html.php
@@ -10,23 +10,6 @@ class Html extends XFCP_Html
 		$xfstt = \XF::options();
 		$vstr = \XF::visitor();
 
-		/*  # Old version, regular expression didn't find domain without ' / ' at the end
-
-			preg_match_all('|//(.*?)/|is', $url, $siteurl);
-
-			if(!empty($siteurl[1][0])) {
-				if (in_array($siteurl[1][0], explode("\n", $xfstt->AnRefWL))) {
-					return $url;
-				} elseif (in_array($siteurl[1][0], explode("\n", $xfstt->AnRefML))) {
-					return str_ireplace($siteurl[1][0], $_SERVER['HTTP_HOST'], $url);
-				} else {
-					if ($xfstt->AnRefHRlink == true): $url = str_replace(' href="', ' href="' . $xfstt->AnRefPrefix, $url);; endif;
-					if ($vstr['user_id'] == 0 && $xfstt->AnRefHGlink): $url = \XF::phrase('AnRefGL'); endif;
-
-				}
-			} 
-		*/
-
 		preg_match('<a href="(.*?)">', $url, $match);
 
 		if(!empty($match)) {
