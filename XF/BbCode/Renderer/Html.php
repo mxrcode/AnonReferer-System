1e8a0e21e50977027edae484374e5cc58a128429
mxrcode <123785508+mxrcode@users.noreply.github.com>
2023-02-15 04:02:20 +0200

    2.2.0.1
    Changed the approach in searching for a domain in a URL, previously the plugin did not work when the link had no path and a slash ' / ' at the end.
    Minor plugin name change from "[MxR] AnonReferrer System" to "[MxR] AnonReferer System".
    
    The update is optional.

--- XF/BbCode/Renderer/Html.php
+++ XF/BbCode/Renderer/Html.php
@@ -8,20 +8,41 @@ class Html extends XFCP_Html
 	{
 		$url = parent::renderTagUrl($children, $option, $tag, $options);
 		$xfstt = \XF::options();
-		$visitor = \XF::visitor();
+		$vstr = \XF::visitor();
 
-		preg_match_all('|//(.*?)/|is', $url, $siteurl);
-		if(!empty($siteurl[1][0])) {
-			if (in_array($siteurl[1][0], explode("\n", $xfstt->AnRefWL))) {
+		/*  # Old version, regular expression didn't find domain without ' / ' at the end
+
+			preg_match_all('|//(.*?)/|is', $url, $siteurl);
+
+			if(!empty($siteurl[1][0])) {
+				if (in_array($siteurl[1][0], explode("\n", $xfstt->AnRefWL))) {
+					return $url;
+				} elseif (in_array($siteurl[1][0], explode("\n", $xfstt->AnRefML))) {
+					return str_ireplace($siteurl[1][0], $_SERVER['HTTP_HOST'], $url);
+				} else {
+					if ($xfstt->AnRefHRlink == true): $url = str_replace(' href="', ' href="' . $xfstt->AnRefPrefix, $url);; endif;
+					if ($vstr['user_id'] == 0 && $xfstt->AnRefHGlink): $url = \XF::phrase('AnRefGL'); endif;
+
+				}
+			} 
+		*/
+
+		preg_match('<a href="(.*?)">', $url, $match);
+
+		if(!empty($match)) {
+			$domain = parse_url($match[1], PHP_URL_HOST);
+
+			if (in_array($domain, explode("\n", $xfstt->AnRefWL))) {
 				return $url;
-			} elseif (in_array($siteurl[1][0], explode("\n", $xfstt->AnRefML))) {
-				return str_ireplace($siteurl[1][0], $_SERVER['HTTP_HOST'], $url);
+			} elseif (in_array($domain, explode("\n", $xfstt->AnRefML))) {
+				return str_ireplace($domain, $_SERVER['HTTP_HOST'], $url);
 			} else {
 				if ($xfstt->AnRefHRlink == true): $url = str_replace(' href="', ' href="' . $xfstt->AnRefPrefix, $url);; endif;
-				if ($visitor['user_id'] == 0 && $xfstt->AnRefHGlink): $url = \XF::phrase('AnRefGL'); endif;
+				if ($vstr['user_id'] == 0 && $xfstt->AnRefHGlink): $url = \XF::phrase('AnRefGL'); endif;
 
 			}
 		}
+
 		return $url;
 	}
 }
