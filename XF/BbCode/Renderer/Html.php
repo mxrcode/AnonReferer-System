2bc9fff79c75766fc00bb5742a3c67ee275a1618
mxrcode <123785508+mxrcode@users.noreply.github.com>
2023-02-15 04:00:42 +0200

    2.2.0.0
    Changed the main task of the plugin, now it is Hiding links from guests.
    Ability to disable referer hiding for links without disabling other plugin functions.
    The fix from this post has been moved to the main update branch.
    Extension of plugin support to xenforo 2.2.x version
    Minor fixes in master language phrases.
    Services for hiding referer on links that do not work and do not support https have been removed from the plugin.
    
    Upgrading is optional if you are satisfied with the features of previous versions.

--- XF/BbCode/Renderer/Html.php
+++ XF/BbCode/Renderer/Html.php
@@ -11,13 +11,16 @@ class Html extends XFCP_Html
 		$visitor = \XF::visitor();
 
 		preg_match_all('|//(.*?)/|is', $url, $siteurl);
-		if (in_array($siteurl[1][0], explode("\n", $xfstt->AnRefWL))) {
-			return $url;
-		} elseif (in_array($siteurl[1][0], explode("\n", $xfstt->AnRefML))) {
-			return str_ireplace($siteurl[1][0], $_SERVER['HTTP_HOST'], $url);
-		} else {
-			$url = str_replace(' href="', ' href="' . $xfstt->AnRefPrefix, $url);
-			if ($visitor['user_id'] == 0 && $xfstt->AnRefHGlink): $url = \XF::phrase('AnRefGL'); endif;
+		if(!empty($siteurl[1][0])) {
+			if (in_array($siteurl[1][0], explode("\n", $xfstt->AnRefWL))) {
+				return $url;
+			} elseif (in_array($siteurl[1][0], explode("\n", $xfstt->AnRefML))) {
+				return str_ireplace($siteurl[1][0], $_SERVER['HTTP_HOST'], $url);
+			} else {
+				if ($xfstt->AnRefHRlink == true): $url = str_replace(' href="', ' href="' . $xfstt->AnRefPrefix, $url);; endif;
+				if ($visitor['user_id'] == 0 && $xfstt->AnRefHGlink): $url = \XF::phrase('AnRefGL'); endif;
+
+			}
 		}
 		return $url;
 	}
