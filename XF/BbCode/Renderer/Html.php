cfb0199a1afeca056d0305351fa20ac600f1fcb8
mxrcode <123785508+mxrcode@users.noreply.github.com>
2023-02-16 04:54:09 +0200

    Update Html.php
    Code style

--- XF/BbCode/Renderer/Html.php
+++ XF/BbCode/Renderer/Html.php
@@ -10,24 +10,24 @@ class Html extends XFCP_Html
 		$options = \XF::options();
 		$visitor = \XF::visitor();
 
-        preg_match('/<a href="(.*?)">/i', $url, $matches);
+		preg_match('/<a href="(.*?)">/i', $url, $matches);
 
-        if (!empty($matches)) {
-            $domain = parse_url($matches[1], PHP_URL_HOST);
+		if (!empty($matches)) {
+			$domain = parse_url($matches[1], PHP_URL_HOST);
 
-            if (in_array($domain, explode("\n", $options->AnRefWL))) {
-                return $url;
-            } elseif (in_array($domain, explode("\n", $options->AnRefML))) {
-                return str_ireplace($domain, $_SERVER['HTTP_HOST'], $url);
-            } else {
-                if ($options->AnRefHRlink) {
-                    $url = str_replace(' href="', ' href="' . $options->AnRefPrefix, $url);
-                }
-                if ($visitor['user_id'] == 0 && $options->AnRefHGlink) {
-                    $url = \XF::phrase('AnRefGL');
-                }
-            }
-        }
+			if (in_array($domain, explode("\n", $options->AnRefWL))) {
+				return $url;
+			} elseif (in_array($domain, explode("\n", $options->AnRefML))) {
+				return str_ireplace($domain, $_SERVER['HTTP_HOST'], $url);
+			} else {
+				if ($options->AnRefHRlink) {
+					$url = str_replace(' href="', ' href="' . $options->AnRefPrefix, $url);
+				}
+				if ($visitor['user_id'] == 0 && $options->AnRefHGlink) {
+					$url = \XF::phrase('AnRefGL');
+				}
+			}
+		}
 
 		return $url;
 	}
