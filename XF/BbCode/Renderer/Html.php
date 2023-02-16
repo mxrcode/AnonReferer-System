a18fadade1959a0237fdbd42896ca8386ba2dd3d
mxrcode <123785508+mxrcode@users.noreply.github.com>
2023-02-16 04:53:10 +0200

    Update Html.php

--- XF/BbCode/Renderer/Html.php
+++ XF/BbCode/Renderer/Html.php
@@ -7,24 +7,27 @@ class Html extends XFCP_Html
 	public function renderTagUrl(array $children, $option, array $tag, array $options)
 	{
 		$url = parent::renderTagUrl($children, $option, $tag, $options);
-		$xfstt = \XF::options();
-		$vstr = \XF::visitor();
+		$options = \XF::options();
+		$visitor = \XF::visitor();
 
-		preg_match('<a href="(.*?)">', $url, $match);
+        preg_match('/<a href="(.*?)">/i', $url, $matches);
 
-		if(!empty($match)) {
-			$domain = parse_url($match[1], PHP_URL_HOST);
+        if (!empty($matches)) {
+            $domain = parse_url($matches[1], PHP_URL_HOST);
 
-			if (in_array($domain, explode("\n", $xfstt->AnRefWL))) {
-				return $url;
-			} elseif (in_array($domain, explode("\n", $xfstt->AnRefML))) {
-				return str_ireplace($domain, $_SERVER['HTTP_HOST'], $url);
-			} else {
-				if ($xfstt->AnRefHRlink == true): $url = str_replace(' href="', ' href="' . $xfstt->AnRefPrefix, $url);; endif;
-				if ($vstr['user_id'] == 0 && $xfstt->AnRefHGlink): $url = \XF::phrase('AnRefGL'); endif;
-
-			}
-		}
+            if (in_array($domain, explode("\n", $options->AnRefWL))) {
+                return $url;
+            } elseif (in_array($domain, explode("\n", $options->AnRefML))) {
+                return str_ireplace($domain, $_SERVER['HTTP_HOST'], $url);
+            } else {
+                if ($options->AnRefHRlink) {
+                    $url = str_replace(' href="', ' href="' . $options->AnRefPrefix, $url);
+                }
+                if ($visitor['user_id'] == 0 && $options->AnRefHGlink) {
+                    $url = \XF::phrase('AnRefGL');
+                }
+            }
+        }
 
 		return $url;
 	}
