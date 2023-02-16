21c7c131bf53f720ceb8225b7747c9a890245994
mxrcode <123785508+mxrcode@users.noreply.github.com>
2023-02-16 04:51:48 +0200

    Revert "Update Html.php"
    This reverts commit 1364d86bba63bc614977d8ce5945fc4d0b677781.

--- XF/BbCode/Renderer/Html.php
+++ XF/BbCode/Renderer/Html.php
@@ -4,27 +4,28 @@ namespace MxR\AnonReferrer\XF\BbCode\Renderer;
 
 class Html extends XFCP_Html
 {
-    public function renderTagUrl(array $children, $option, array $tag, array $options)
-    {
-        $url = parent::renderTagUrl($children, $option, $tag, $options);
-        $xf_options = \XF::options();
-        $visitor = \XF::visitor();
-        
-        // Extract the domain from the URL using parse_url function
-        $url_parts = parse_url($url);
-        $domain = $url_parts['host'] ?? '';
-        
-        if (!empty($domain)) {
-            if (in_array($domain, explode("\n", $xf_options->AnRefWL))) {
-                return $url;
-            } elseif (in_array($domain, explode("\n", $xf_options->AnRefML))) {
-                return str_ireplace($domain, $_SERVER['HTTP_HOST'], $url);
-            } else {
-                if ($xf_options->AnRefHRlink): $url = str_replace(' href="', ' href="' . $xf_options->AnRefPrefix, $url); endif;
-                if ($visitor['user_id'] == 0 && $xf_options->AnRefHGlink): $url = \XF::phrase('AnRefGL'); endif;
-            }
-        }
+	public function renderTagUrl(array $children, $option, array $tag, array $options)
+	{
+		$url = parent::renderTagUrl($children, $option, $tag, $options);
+		$xfstt = \XF::options();
+		$vstr = \XF::visitor();
 
-        return $url;
-    }
+		preg_match('<a href="(.*?)">', $url, $match);
+
+		if(!empty($match)) {
+			$domain = parse_url($match[1], PHP_URL_HOST);
+
+			if (in_array($domain, explode("\n", $xfstt->AnRefWL))) {
+				return $url;
+			} elseif (in_array($domain, explode("\n", $xfstt->AnRefML))) {
+				return str_ireplace($domain, $_SERVER['HTTP_HOST'], $url);
+			} else {
+				if ($xfstt->AnRefHRlink == true): $url = str_replace(' href="', ' href="' . $xfstt->AnRefPrefix, $url);; endif;
+				if ($vstr['user_id'] == 0 && $xfstt->AnRefHGlink): $url = \XF::phrase('AnRefGL'); endif;
+
+			}
+		}
+
+		return $url;
+	}
 }
